7294f53e1d4c0bab6686e7494cddb4e9c166c7bc
Fahmi <muhammad.attaqwa@students.amikom.ac.id>
2020-07-06 18:21:09 +0700

    class produk

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectProduk
+{
+    class Program
+    {
+        static List<Produk> daftarProduk = new List<Produk>();
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Responsi UAS Matakuliah Pemrograman";
+            bool loop = true;
+            while (loop)
+            {
+                TampilMenu();
+
+                Console.Write("\nNomor Menu [1..4]: ");
+                int nomorMenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (nomorMenu)
+                {
+                    case 1:
+                        TambahProduk();
+                        break;
+
+                    case 2:
+                        HapusProduk();
+                        break;
+
+                    case 3:
+                        TampilProduk();
+                        break;
+
+                    case 4:
+                        loop = false;
+                        break;
+
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Maaf, menu yang anda pilih tidak tersedia");
+                        break;
+                }
+            }
+        }
+
+        static void TampilMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Pilih menu Aplikasi");
+            Console.WriteLine("\n1. Tambah Produk");
+            Console.WriteLine("2. Hapus Produk");
+            Console.WriteLine("3. Tampilkan Produk");
+            Console.WriteLine("4. Keluar");
+        }
+
+        static void TambahProduk()
+        {
+            Console.Clear();
+
+            Produk product = new Produk();
+            Console.WriteLine("Tambah Data Produk");
+            Console.Write("\nKode Produk : ");
+            product.codeproduct = Console.ReadLine();
+            Console.Write("Nama Produk : ");
+            product.Nameproduct = Console.ReadLine();
+            Console.Write("Harga Beli : ");
+            product.ProductBuy = double.Parse(Console.ReadLine());
+            Console.Write("Harga Jual : ");
+            product.ProductSell = double.Parse(Console.ReadLine());
+
+            daftarProduk.Add(product);
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+        }
+
+        static void HapusProduk()
+        {
+            Console.Clear();
+
+            int fa = -1, delete = -1;
+            Console.WriteLine("Hapus Data Produk");
+            Console.Write("Kode Produk : ");
+            string code = Console.ReadLine();
+            foreach (Produk product in daftarProduk)
+            {
+                fa++;
+                if(product.codeproduct == code)
+                {
+                    delete = fa;
+                }
+            }
+            if (delete != -1)
+            {
+                daftarProduk.RemoveAt(delete);
+                Console.WriteLine("\nData produk berhasil di hapus");
+            }
+            else
+            {
+                Console.WriteLine("\nKode produk tidak ditemukan");
+            }
+
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+        }
+
+        static void TampilProduk()
+        {
+            Console.Clear();
+
+            int noUrut = 0;
+            Console.WriteLine("Data Produk");
+            foreach (Produk product in daftarProduk)
+            {
+                noUrut++;
+                Console.WriteLine("{0}. Kode Produk: {1}, Nama Produk: {2}, Harga Beli: {3}, Harga Jual: {4}", noUrut, product.codeproduct, product.Nameproduct, product.ProductBuy, product.ProductSell);
+            }
+            if (noUrut < 1)
+            {
+                Console.WriteLine("Data Produk Kosong");
+            }
+
+            Console.WriteLine("\nTekan enter untuk kembali ke menu");
+            Console.ReadKey();
+        }
+    }
+}
