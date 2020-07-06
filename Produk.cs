2d03c36ff527baca992ef12113f184bf0c90c9a9
Fahmi <muhammad.attaqwa@students.amikom.ac.id>
2020-07-06 18:22:31 +0700

    class produk

---
+++ Produk.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectProduk
+{
+    public class Produk
+    {
+        public string codeproduct { get; set; }
+        public string Nameproduct { get; set; }
+        public double ProductBuy { get; set; }
+        public double ProductSell { get; set; }
+
+    }
+}
